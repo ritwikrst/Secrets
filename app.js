199f6d15cf8befcf828be81503a332a4e8842801
RITWIK MUKHERJEE <ritwikrst@gmail.com>
2022-09-19 20:06:43 +0530

    Level_1_auth_stored the u_name & p_word into mong

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -11,6 +12,15 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+
+mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
+const userSchema = {
+  email: String,
+  password: String
+};
+
+const User = new mongoose.model("User", userSchema);
+
 
 
 app.get("/", function (req, res) {
@@ -25,9 +35,34 @@ app.get("/register", function (req, res) {
   res.render("register");
 });
 
+app.post("/register", function (req, res) {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password
+  });
+  newUser.save(function (err) {
+    if (err) {
+      console.log(err);
+    }
+    else {
+      res.render("secrets");
+    }
+  });
+});
 
-
-
+app.post("/login", function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+  User.findOne({ email: username }, function (err, foundUser) {
+    if (err)
+      console.log(err);
+    else {
+      if (foundUser.password === password) {
+        res.render("secrets");
+      }
+    }
+  })
+})
 
 
 
