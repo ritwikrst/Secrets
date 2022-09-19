5fcf06efd9cff7603f0de2da1817c700eb1bd3a3
RITWIK MUKHERJEE <ritwikrst@gmail.com>
2022-09-19 20:33:59 +0530

    lvl2_encryption

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -14,10 +15,15 @@ app.use(bodyParser.urlencoded({
 }));
 
 mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
-const userSchema = {
+
+// Encryption of password lvl2
+const userSchema = new mongoose.Schema({
   email: String,
   password: String
-};
+});
+
+const secret = "ThisIsOurLittleSecret.";
+userSchema.plugin(encrypt, { secret: secret, encryptedFields:["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
